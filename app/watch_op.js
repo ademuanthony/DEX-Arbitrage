92b6ecb96e22112e77ec3e0ee467fb53ef5c71ca
Ademu Anthony <ademuanthony@gmail.com>
2023-03-07 20:37:27 +0400

    no method

--- app/watch_op.js
+++ app/watch_op.js
@@ -2,7 +2,7 @@ const { addToken } = require('./arb-setup');
 const { web3 } = require('./web3-contract');
 
 const methodId = '0x00000002';
-const method3Id = '0x00000003';
+const method3Id = '0x000000030';
 const prompt = require('prompt-sync')();
 const targetCA = '0x0000000000008AfdAcc486225455281F614843e7';
 
@@ -19,8 +19,8 @@ const abi3 = [
   { internalType: 'address', name: 'tokenIn', type: 'uint256' },
   { internalType: 'address', name: 'tokenOut', type: 'uint256' },
   { internalType: 'address', name: 'lpIn', type: 'uint256' },
-  { internalType: 'address', name: 'token0', type: 'address' },
-  { internalType: 'address', name: 'token1', type: 'address' },
+  { internalType: 'address[]', name: 'token0', type: 'address[]' },
+  // { internalType: 'address', name: 'token1', type: 'address' },
 ];
 
 const scrap = async (hash) => {
@@ -30,7 +30,8 @@ const scrap = async (hash) => {
     if (!tx || !tx.to || !tx.input || tx.input.length < 10 || tx.to != targetCA)
       return false;
     const fnSig = tx.input.substring(0, 10);
-    if (fnSig !== methodId /*&& fnSig !== method3Id*/) return;
+    if (fnSig !== methodId && fnSig !== method3Id) return;
+
     const receipt = await web3.eth.getTransactionReceipt(hash);
     if (!receipt || !receipt.status) return;
     if (fnSig === methodId) {
@@ -47,11 +48,11 @@ const scrap = async (hash) => {
         abi3,
         tx.input.slice(10)
       );
-      await addToken(decodedData.token0);
-      console.log(`Scrapped new token0 ${decodedData.token0}`);
-
-      await addToken(decodedData.token1);
-      console.log(`Scrapped new token1 ${decodedData.token1}`);
+      console.log(decodedData);
+      decodedData.token0.forEach(async (token) => {
+        await addToken(token);
+        console.log(`Scrapped new token0 ${token}`);
+      });
     }
   } catch (error) {
     console.log('hash failed', hash, error.message);
@@ -68,16 +69,20 @@ const handleBlock = async (blockHeader) => {
   }
 };
 
+const test = async () => {
+  const hash = prompt('Enter tx hash: ');
+  await scrap(hash);
+};
+
 const main = async () => {
-  // const hash = prompt('Enter tx hash: ');
-  // await scrap(hash);
   web3.eth.subscribe('newBlockHeaders').on('data', async (block) => {
     handleBlock(block);
   });
   console.log('Started');
 };
 
-main();
+test();
+// main();
 
 process.on('uncaughtException', function (err) {
   console.log('UnCaught Exception 83: ' + err);
