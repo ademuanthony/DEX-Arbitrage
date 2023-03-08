be5b3a002e5625d4a0c5042b90be207141487dbe
Ademu Anthony <ademuanthony@gmail.com>
2023-03-08 08:33:37 +0400

    target scrap

--- run.js
+++ run.js
@@ -72,7 +72,7 @@ const main = () => {
   const swapEventTopic =
     '0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822';
   const scrapTarget =
-    '0x0000000000000000000000000000000000008AfdAcc486225455281F614843e7';
+    web3.utils.toChecksumAddress('0x0000000000008AfdAcc486225455281F614843e7');
   const logOptions = {
     // Filter transfer topics
     topics: [swapEventTopic],
